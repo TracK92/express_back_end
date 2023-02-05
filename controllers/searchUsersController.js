0240f531ffe9b338d31305bc1b2d3d5d17615f84
kadenyi <treykadenyi@gmail.com>
2023-02-05 15:15:58 +0300

    Edit auth token

--- controllers/searchUsersController.js
+++ controllers/searchUsersController.js
@@ -2,22 +2,19 @@ const db = require("../firebase.js");
 const { uuid } = require("uuidv4");
 const { doc, setDoc, getDoc } = require("firebase/firestore");
 const { Octokit } = require("@octokit/core");
+const authToken = process.env.GITHUB_TOKEN;
 /* 
   @route: GET /searchUsers
-  @desc: Validate access code
+  @desc: Search for user profiles
   @access: Public
 */
 const searchUsers = async (req, res) => {
- const octokit = new Octokit({
-  auth: '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'
-})
+  const octokit = new Octokit({
+    auth: authToken,
+  });
 
-  const response = await octokit.request('GET /repos/{owner}/',
-    {
-      owner: 'TracK92',
-    }
-  )
-  
+  const response = await octokit.request('GET /search/users?q=tom+repos:%3E42+followers:%3E1000', {});
+
   res.status(200).json({
     success: true,
     data: response.data,
