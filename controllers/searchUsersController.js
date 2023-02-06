da734b8c0eccf622d18f3875fdfe9a9f9669690e
kadenyi <treykadenyi@gmail.com>
2023-02-06 15:41:17 +0300

    Create the searchUser method

--- controllers/searchUsersController.js
+++ controllers/searchUsersController.js
@@ -8,22 +8,48 @@ const authToken = process.env.GITHUB_TOKEN;
   @desc: Search for user profiles
   @access: Public
 */
+
+
+//   (GET) searchGithubUsers
+// https://api.github.com/search/users
+// Parameters: q (search term), page (page number), per_page (results per page)
+// Return: an array of users with login name that contains the search term
+
 const searchUsers = async (req, res) => {
   const octokit = new Octokit({
     auth: authToken,
   });
 
-  const response = await octokit.request('GET /search/users?q={owner}+followers:{followers}', {
-    owner: 'TracK92',
-    followers: '>4',
-  });
+  // get users with parameters q, page, per_page
 
+  const response = await octokit.request("GET /search/users", {
+    q: req.query.q,
+    page: req.query.page,
+    per_page: req.query.per_page,
+  });
 
   res.status(200).json({
     success: true,
     data: response.data,
   });
 };
+
+// const searchUsers = async (req, res) => {
+//   const octokit = new Octokit({
+//     auth: authToken,
+//   });
+
+//   const response = await octokit.request('GET /search/users?q={owner}+followers:{followers}', {
+//     owner: 'TracK92',
+//     followers: '>4',
+//   });
+
+
+//   res.status(200).json({
+//     success: true,
+//     data: response.data,
+//   });
+// };
 
 module.exports = {
   searchUsers,
